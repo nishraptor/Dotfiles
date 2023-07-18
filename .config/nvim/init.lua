e628f246edf835eee7b667c0d7e0ad589296d7dd
nishraptor <nishraptor@gmail.com>
2023-07-18 13:56:53 -0700

    Updated nvim-cmp
    Signed-off-by: nishraptor <nishraptor@gmail.com>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,41 +1,245 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
+vim.opt.termguicolors = true
+vim.opt.mousemoveevent = true
+--vim.opt.number = true
+--vim.opt.cursorline = true
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 plugins = {
-{"catppuccin/nvim", name = "catppuccin", priority = 1000, opts = {integrations = {nvimtree=true}}},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opts = {
+			integrations = {
+				nvimtree = true,
+				treesitter = true,
+				gitsigns = true,
+				telescope = {
+					enabled = true,
+					-- style = "nvchad"
+				},
+			},
+		},
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = true,
+	},
+	{
+		"akinsho/bufferline.nvim",
+		version = "*",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		lazy = false,
+		config = function()
+			local mocha = require("catppuccin.palettes").get_palette("mocha")
+			require("bufferline").setup({
+				highlights = require("catppuccin.groups.integrations.bufferline").get({
+					styles = { "italic", "bold" },
+					custom = {
+						all = {
+							fill = { bg = mocha.base },
+						},
+						mocha = {
+							background = { fg = mocha.text },
+						},
+						latte = {
+							background = { fg = "#000000" },
+						},
+					},
+				}),
+				options = {
+					hover = {
+						enabled = true,
+						delay = 50,
+						reveal = { "close" },
+					},
+					offsets = {
+						{
+							filetype = "NvimTree",
+							text = "File Explorer",
+							highlight = "Directory",
+							separater = true,
+						},
+					},
+					separator_style = "thick",
+				},
+			})
+		end,
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		opts = {
+			show_current_context = true,
+			show_current_context_start = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			lspconfig.pyright.setup({ capabilities = capabilities })
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.clangd.setup({ capabilities = capabilities })
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		build = ":MasonUpdate",
+		opts = { PATH = "prepend" },
+	}, -- :MasonUpdate updates registry contents
+	{
+		"williamboman/mason-lspconfig",
+		opts = {
+			ensure_installed = { "lua_ls", "pyright", "clangd" },
+		},
+	},
 
-{"nvim-tree/nvim-tree.lua",
-  version = "*",
-  lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = true
-},
+	{
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"jose-elias-alvarez/null-ls.nvim",
+		},
+		config = { ensure_installed = { "stylua", "black", "flake8" }, handlers = {} },
+	},
 
-{"neovim/nvim-lspconfig"},
-{"dundalek/lazy-lsp.nvim", opts = {}},
-{"nvim-lualine/lualine.nvim", opts = {theme = 'catppuccin'}},
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		config = function()
+			local null_ls = require("null-ls")
+			--null_ls.setup({sources = sources})
+			null_ls.setup({
+				sources = { null_ls.builtins.formatting.astyle },
+				-- you can reuse a shared lspconfig on_attach callback here
+				on_attach = function(client, bufnr)
+					if client.supports_method("textDocument/formatting") then
+						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+						vim.api.nvim_create_autocmd("BufWritePre", {
+							group = augroup,
+							buffer = bufnr,
+							callback = function()
+								-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+								-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+								--vim.lsp.buf.formatting_sync()
+								vim.lsp.buf.format({ async = false })
+							end,
+						})
+					end
+				end,
+			})
+		end,
+	},
+	--{'mawkler/modicator.nvim', dependencies = "catppuccin/nvim", config = true, lazy = false},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
 
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+
+			configs.setup({
+				ensure_installed = { "c", "lua", "python", "cmake", "bash", "json", "cpp" },
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {}, -- this is equalent to setup({}) function
+	},
+
+	{
+		"nvimdev/lspsaga.nvim",
+		event = "LspAttach",
+		config = function()
+			require("lspsaga").setup({})
+		end,
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter", -- optional
+			"nvim-tree/nvim-web-devicons", -- optional
+		},
+	},
+	{ "karb94/neoscroll.nvim", config = true },
+	{
+		"numToStr/Comment.nvim",
+		lazy = false,
+		opts = {
+			toggler = { line = "<C-_>" },
+			opleader = { block = "<C-_>" },
+		},
+	},
+	{ "lewis6991/gitsigns.nvim", config = true },
+	{ "L3MON4D3/LuaSnip" },
+
+	{
+		"/hrsh7th/nvim-cmp",
+		dependencies = { "hrsh7th/cmp-nvim-lsp", "saadparwaiz1/cmp_luasnip" },
+		config = function()
+			local cmp = require("cmp")
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				}),
+			})
+		end,
+	},
+	--{ "lukas-reineke/virt-column.nvim", config = true,                  lazy = false },
+	{ "nvim-lualine/lualine.nvim", opts = { theme = "catppuccin" } },
 }
 
 vim.g.mapleader = " " --leaderkey
 
 require("lazy").setup(plugins)
 
+vim.cmd.colorscheme("catppuccin")
 
-vim.cmd.colorscheme "catppuccin"
+-- Setup Language Servers
+--local lspconfig = require("lspconfig")
+--lspconfig.pyright.setup({})
+--lspconfig.lua_ls.setup({})
+--lspconfig.clangd.setup{}
 
 -- vim options
 vim.opt.tabstop = 2
@@ -50,3 +254,49 @@ vim.keymap.set("n", "<leader><leader>", "<cmd>w<CR>")
 
 -- nvim-tree
 vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>")
+vim.keymap.set("n", "<S-l>", "<cmd>:bnext<CR>")
+vim.keymap.set("n", "<S-h>", "<cmd>:bprev<CR>")
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+--telescope
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
+--LSP config
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		-- Enable completion triggered by <c-x><c-o>
+		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+		-- Buffer local mappings.
+		-- See `:help vim.lsp.*` for documentation on any of the below functions
+		local opts = { buffer = ev.buf }
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+		--vim.keymap.set('n', 'gd', "<Cmd>Lspsaga lsp_finder<CR>", opts)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+		vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+		vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+		vim.keymap.set("n", "<space>wl", function()
+			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		end, opts)
+		vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+		vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+		vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+		vim.keymap.set("n", "<space>f", function()
+			vim.lsp.buf.format({ async = true })
+		end, opts)
+	end,
+})
